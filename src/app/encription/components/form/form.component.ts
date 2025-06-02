8698ecc21b2cc8fbff770dc076fe660db7e55cb1
Larissa <dejamepienso@gmail.com>
2025-06-02 14:58:28 -0600

    Update layout

--- src/app/encription/components/form/form.component.ts
+++ src/app/encription/components/form/form.component.ts
@@ -1,9 +1,7 @@
 import { Component, input, inject, signal } from '@angular/core';
-import { environment } from '@environments/environment.development';
 import { EncriptService } from '../../../services/encript.services';
-import {EncriptResponse} from '../../interfaces/encript.interface'
+import { EncriptResponse } from '../../interfaces/encript.interface';
 import { NgIf } from '@angular/common';
-
 
 @Component({
   selector: 'app-form',
@@ -12,12 +10,10 @@ import { NgIf } from '@angular/common';
 })
 export class AppFormComponent {
   textTyped = input<string>();
-  imageUrl: string = environment.imageGif;
   EncriptService = inject(EncriptService);
   encriptedResponse = signal<EncriptResponse | null>(null);
 
   onTextTyped(query: string) {
     this.EncriptService.encryptText(query);
-
   }
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { AppFormComponent } from 'src/app/encription/components/form/form.component';
+import { environment } from '@environments/environment.development';
 
 @Component({
   selector: 'app-home-page',
@@ -7,5 +8,5 @@ import { AppFormComponent } from 'src/app/encription/components/form/form.compon
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
-
+  imageUrl: string = environment.imageGif;
 }

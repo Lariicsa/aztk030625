c25a275418af33e4fd1ee7ec3412545dd9f07d9c
Larissa <dejamepienso@gmail.com>
2025-06-03 20:09:57 -0600

    Remove useless files

--- src/app/encryption/interfaces/encript.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface EncriptResponse {
-  response: string;
-  encrypted: string;
-}
--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  //urls
-  imageGif: 'https://www.bazsolicitudunica.com.mx/assets/gifts/hello_pwa.gif',
-
-  //Keys
-  apiUrl: 'http://localhost:1987',
-};
